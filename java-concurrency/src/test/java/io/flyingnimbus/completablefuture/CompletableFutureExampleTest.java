ef09a0760f42bcab406431165ae9fd74458e04ad
Kye Yeung <yeung.kye@pm.me>
2020-11-17 15:26:17 +0000

    adding link for reactive foundation

--- java-concurrency/src/test/java/io/flyingnimbus/completablefuture/CompletableFutureExampleTest.java
+++ java-concurrency/src/test/java/io/flyingnimbus/completablefuture/CompletableFutureExampleTest.java
@@ -1,46 +1,107 @@
 package io.flyingnimbus.completablefuture;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 class CompletableFutureExampleTest {
 
     private final CompletableFutureExample objectUnderTest = new CompletableFutureExample();
 
     @Test
-    public void runAsyncCommonForkJoinPool() {
+    void runAsyncCommonForkJoinPool() {
         objectUnderTest.runAsyncCommonForkJoinPool();
     }
 
     @Test
-    public void runAsyncExecutor() {
+    void runAsyncExecutor() {
         Executor executor = Executors.newSingleThreadExecutor();
         objectUnderTest.runAsyncExecutor(executor);
     }
 
     @Test
-    public void callbacks() {
+    void callbacks() {
         objectUnderTest.callbacks();
     }
 
     @Test
-    public void chainingCompletionStages() throws InterruptedException {
+    void chainingCompletionStages() throws InterruptedException {
         objectUnderTest.chainingCompletionStages();
 
-        TimeUnit.SECONDS.sleep(2);
+        sleep(2);
     }
 
     @Test
-    public void exceptions() throws ExecutionException, InterruptedException {
+    void exceptions() throws ExecutionException, InterruptedException {
         objectUnderTest.exceptions();
     }
 
     @Test
-    public void timeouts() throws ExecutionException, InterruptedException {
+    void timeouts() throws ExecutionException, InterruptedException {
         objectUnderTest.timeouts();
     }
+
+    @Test
+    void supplyAsync() {
+
+        System.out.println("execution 1: thread: " + Thread.currentThread().getName());
+
+        CompletableFuture<String> job1 = CompletableFuture.supplyAsync(() -> {
+
+            sleep(1);
+            System.out.println("supplyAsync: thread: " + Thread.currentThread().getName());
+            return "Job 1";
+        });
+
+        System.out.println("execution 2: thread: " + Thread.currentThread().getName());
+
+        job1.join();
+    }
+
+    @Test
+    void thenAcceptBothAsync() {
+
+        System.out.println("execution 1: thread: " + Thread.currentThread().getName());
+
+        CompletableFuture<String> job1 = CompletableFuture.supplyAsync(() -> {
+            for (int i = 0; i < 6; i++) {
+                sleep(250);
+                System.out.println("supplyAsync1: thread: " + Thread.currentThread().getName());
+            }
+            return "Job 1";
+        });
+
+        CompletableFuture<String> job2 = CompletableFuture.supplyAsync(() -> {
+            for (int i = 0; i < 4; i++) {
+                sleep(250);
+                System.out.println("supplyAsync2: thread: " + Thread.currentThread().getName());
+            }
+            return "Job 2";
+        });
+
+        System.out.println("execution 2: thread: " + Thread.currentThread().getName());
+
+        //NOTE: using either thenAcceptBothAsync pr thenAcceptBoth would have the same  "async"effect as job1 and job2
+        // already use supplyAsync()
+        job1.thenAcceptBothAsync(job2, (job1Result, job2Result) -> {
+            System.out.println("thenAcceptBothAsync: thread: " + Thread.currentThread().getName());
+            System.out.println(job1Result);
+            System.out.println(job2Result);
+        }).join();
+
+        job1.join();
+
+        System.out.println("execution 3: thread: " + Thread.currentThread().getName());
+    }
+
+
+    private void sleep(int i) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(i);
+        } catch (InterruptedException e) {
+            Assertions.fail("this shouldn't have happened", e);
+        }
+    }
 }
+
